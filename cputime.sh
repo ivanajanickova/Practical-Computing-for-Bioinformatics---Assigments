33e4719ce1b704a9b84c84b9e1b5edca17cdd35c
Ivana Janickova <67543976+ivanajanickova@users.noreply.github.com>
2020-10-29 11:47:29 +0100

    Update cputime.sh

--- cputime.sh
+++ cputime.sh
@@ -8,12 +8,12 @@ then
 	#Given Job Name finds a maximum CPU time and assigns the output to a variable cpu_time.
 	#Fist finds files containg the given job name, extracting the file name form the output.
 	#Searches for CPU time data in the filtered files and sorts the time data. 
-	#Chosses the longest CPU time, isolates the time data and stors the output to the variable cpu_time.
+	#Choses the longest CPU time, isolates the time data and stores the output to the variable cpu_time.
 	cpu_time=$(grep $1 * | grep "Job Name: " | cut -d ":" -f1 | xargs cat | grep "cput" | sort | tail -1 | awk -F '[=,]' '{print $2}')
 	echo "CPU time of " $cpu_time " for job " $1 
 else
 	#Searches and isolates the CPU time data. Sorts and assingns the longest time to the variable max_time.
-	#Using the max_time searches for the belonging job name and saves it the the variable job_name_max. 
+	#Using the max_time searches for the corresponding job name and saves it the the variable job_name_max. 
 	max_time=$(grep 'cput' * | awk -F '[=,]' '{print $2}' | sort | tail -1)
 	job_name_max=$(egrep -lr $max_time * | xargs  cat | grep "Job Name: " | cut -d ":" -f2)
 	echo "Maximum CPU time of " $max_time " for job " $job_name_max
